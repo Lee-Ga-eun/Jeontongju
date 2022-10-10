883294b5342c634525dc737cd6ec24048baf3663
jmh0434 <80581335+jmh0434@users.noreply.github.com>
2022-10-10 23:54:32 +0900

    삭제

--- react-project/src/Routes/Result.js
+++
@@ -1,8 +0,0 @@
-function Result(){
-    return(
-        <div>
-            결과 페이지입니다.
-        </div>
-    )
-}
-export default Result;
--- react-project/src/Routes/Stores.js
+++
@@ -1,8 +0,0 @@
-function Stores(){
-    return(
-        <div>
-            가게 페이지입니다.
-        </div>
-    );
-}
-export default Stores;
