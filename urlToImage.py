daee77f22e40c0fcd31cd85cb4d33cd5c3fb4bb9
Gaeun Lee <gaeun0800@gmail.com>
2022-10-12 19:26:29 +0900

    url-> 이미지 변환 코드

--- urlToImage.py
+++ urlToImage.py
@@ -2,6 +2,8 @@ from urllib.request import urlopen
 import pandas as pd
 import csv
 from PIL import Image
+from urllib.error import HTTPError 
+
 
 # 1. 모든 inner.csv 파일 행을 읽는다
 # 2. 한 행마다, url을 찾고 이 url을 이미지로 변경한 것을 img 변수에 저장한다
@@ -12,29 +14,43 @@ from PIL import Image
 f=open('uploads/inner.csv') # csv 파일 읽기
 #print(f)
 data=csv.reader(f)
+check=[] # 빠진 숫자 확인
 
 def urlToImage():
     print(next(data)) # 첫번째 행은 컬럼명이므로 생략
 
     while(True): # 행이 끝날 때까지 반복
-
-        if next(data)=='': # 더이상 출력할 행이 없으면 종료
-            break 
+       
         present=next(data) # 현재 작업하는 행 이름: present
-        print(present)
+
+        if present=='': # 더이상 출력할 행이 없으면 종료
+            continue 
         name=present[1].rstrip() # 전통주명
         url_=present[17] # url 주소
 
+        try: 
+            img=Image.open(urlopen(url_)) # 이미지로 변환 완료
+            check.append(int(present[0])) # 이미지 변환 완료된 고유값 저장
+        except HTTPError as e:
+            print(e)
+            print("오류가 발생한 전통주 명",name)
+
         img=Image.open(urlopen(url_)) # 이미지로 변환 완료
-        img.save('uploads/images/'+name+'.BMP','BMP') # 전통주명과 일치하게 이미지 변환
+        img.save('uploads/images_jeontongju/'+name+'.BMP','BMP') # 전통주명과 일치하게 이미지 변환
+        # db에 넣기
+        #
+
+        if present[0]=='338': # 마지막 행 종료
+            img.save('uploads/images_jeontongju/'+name+'.BMP','BMP') # 전통주명과 일치하게 이미지 변환
+            # db에 넣기
+            #
+            break
 
         # while문 종료
     
     return 0
 
 urlToImage() #실행#
-
-
 
 
 
